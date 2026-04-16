b31b8fd55b9375e9c0c011aa200ef4e9470831f0
brysonddelozier <delozierbryson@gmail.com>
2026-04-16 15:55:06 -0500

    Step 1 test

---
+++ HendrixCollege/Pages/Students/Details.cshtml.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using HendrixCollege.Data;
+using HendrixCollege.Models;
+
+namespace HendrixCollege.Pages.Students
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly HendrixCollege.Data.SchoolContext _context;
+
+        public DetailsModel(HendrixCollege.Data.SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public Student Student { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var student = await _context.Students.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (student is not null)
+            {
+                Student = student;
+
+                return Page();
+            }
+
+            return NotFound();
+        }
+    }
+}
